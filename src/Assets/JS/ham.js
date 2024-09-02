e5537a7f07a35bee7cd7b2319e3c7b94fcb8bf86
Adarsh Pandey <adarshpandey022@gmail.com>
2024-09-02 19:42:43 +0530

    upgrade

---
+++ src/Assets/JS/ham.js
@@ -0,0 +1,10 @@
+$('.hambergur').on('click', () => {
+  $('.fullscreen').toggleClass('active').removeClass('reverse_anim');
+});
+
+$('.close1').on('click', () => {
+  $('.fullscreen').toggleClass('reverse_anim');
+});
+$('.close2').on('click', () => {
+  $('.fullscreen').toggleClass('reverse_anim');
+});
